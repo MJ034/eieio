537e9c926ec79c30005ffe36691332339d81440b
Moreblessing Ngwenya <moreblessingngwenya@squeaky.energy>
2023-09-07 16:39:43 +0200

    qwueies

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,24 @@ import CallToActionButton from '@/components/CallToActionButton'
 import SplitImageLeft from '@/components/SplitImageLeft'
 import SplitButtonLeft from '@/components/SplitButtonLeft'
 import Hero from '@/components/Hero'
+import { createClient } from '@prismicio/client'
 
-export const metadata = {
-  title: 'EIE - Engineering Intelligent Environments',
-  description:
-    'Farming solutions start-up that aims to revolutionize the way farmers work.',
+const queryHomepage = () => {
+  const client = createClient('mjrepo')
+  return client.getSingle('home_page')
+}
+export  async function generateMetadata() {
+  const page = await queryHomepage()
+  return {
+    title: 'EIE - Engineering Intelligent Environments',
+    description:
+      'Farming solutions start-up that aims to revolutionize the way farmers work.',
+  }
 }
 
-export default function Home() {
+export default async function Home() {
+  const page = await queryHomepage();
+  console.log(page.data.slices)
   return (
     <main>
       <Hero />
